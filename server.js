c05bd67e9bff02254275723a5936edd78b543d1b
Zein-Fakhreddine <Zein.Fakhreddine@gmail.com>
2017-02-26 21:40:59 +0300

    removed socket.io

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express'),
     app = express(),
     server = require('http').Server(app),
-    socket = require('socket.io')(server),
     port = Number(process.env.PORT || 8000),
     routes = require('./routes/index'),
     Session = require('./Session.js');
